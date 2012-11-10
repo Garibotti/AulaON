ec9be3060c6b159a6cd774ddcbc50c1720a5eeaa
Enerijr <eneri.junior@yahoo.com.br>
2012-11-10 15:50:03 -0200

    Novo Login

--- site/Login.php
+++ site/Login.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
-
 <html>
 <head>
 	<script type="text/javascript" language="javascript">
-        function passuser() {
-            for (var i = 0; i < document.profalun.ap.length; i++) {
-                if (document.profalun.ap[i].checked) {
-                    direcionar = document.profalun.ap[i].value;
-                }
-            }
-            if (direcionar == "professor") {
-                location = "SProfessor.php";
-            }
-            else
-                location = "SAluno.php";
-        }
-        function alerta() {
+        function passuser() 
+		{
+			location = "http://localhost/Login_exec.php";
+		}
+		
+        function alerta() 
+		{
             alert("Burro!")
         }
     </script>
@@ -29,23 +22,20 @@
     <div id="principal">
 	Nome de Usuário ou E-mail:
     <br />
+	<form method="POST" action="login.php" >
     <input type="text" name="Login" size="24"/><br />
     <br />
+     Senha:
     <br />
-    Senha:
-    <br /> 
     <input type="password" name="Senha" size="24"/><br />
+	<input type="submit" value="Entrar"/>
     <br />
-    <a onclick="alerta()" href="">Esqueci minha senha</a><br />
-    <br />
-    <form name="profalun"> 
-    <input type="radio" name="ap" value="professor" checked="checked"/> Sou Professor 
-    <br />
-    <input type="radio" name="ap" value="aluno"/> Sou Aluno
     <br /> 
     <br />  
     </form> 
-    <input type="submit" name="Entrar" value="Entrar" onclick="passuser()"/>
+    
+	    <br />
+    <a onclick="alerta()" href="">Esqueci minha senha</a><br />
     </div>
     </div>
     <!--/conteudo-->
