39cb663a92444625fd507e76022ccacc84ac148d
Daniel Martín Garibotti <daniel.garibotti@acad.pucrs.br>
2012-11-14 13:01:03 -0200

    Combo Box Compra Funcionando

--- site/Comprar.php
+++ site/Comprar.php
@@ -15,22 +15,23 @@
     	<h2>Compre créditos e veja sua informações referentes a compras</h2>    	
     	<div class="NFSelect" style="z-index: 999;">
     		<img src="imagens_adm_aluno/0.png" class="NFSelectLeft">
-    			<div class="NFSelectRight">R$ 5
-    			</div>
+    			<div class="NFSelectRight">R$ 10</div>
     			<div class="NFSelectTarget" style="display: none; width: 128px;">
     				<ul class="NFSelectOptions" style="width: 95px;">
-		    			<?php for($i=10; $i<=500; $i++){?>
+		    			<?php for($i=10; $i<=500; $i+=10){?>		    				
 			    			<li>
-			    				<a href="javascript:;">R$ <?php echo $i ?></a>
+			    				<a id="<?php echo $i ?>" class="<?php echo  ($i==10) ? "NFOptionActive":"";?>" href="javascript:;">R$ <?php echo $i ?></a>
 			    			</li>
-			    		<?php }?>	    			
+			    		<?php }?>    			
 	    			</ul>
     			</div>
     	</div>
      	<img class="none" title="botao_comprar" src="Imagens_Adm_Aluno/botao_comprar.gif" alt="Comprar">
     </div>
     <br />
-    
+    <form action="" method="post">
+    	<input type="hidden" name="valorDeCompra" id="valorDeCompra" value="10" />
+    </form>
     </div>
     <!--/conteudo-->
 
---
+++ site/Javascript/adm_aluno/FormAluno.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.NFSelectRight').click(function(){		
+		if($('.NFSelectTarget:hidden').length > 0){
+			$('.NFSelectTarget:hidden').show();
+		}else{
+			$('.NFSelectTarget:visible').hide();
+		}		
+	});	
+	
+	$('.NFSelectOptions a').click(function(){		
+		var valorSelecionado = $(this).attr("id");
+		$('.NFOptionActive').removeClass('NFOptionActive');
+		$(this).addClass('NFOptionActive');
+		$('.NFSelectRight').html("R$"+valorSelecionado);
+		$('.NFSelectTarget:visible').hide();
+		$('#valorDeCompra').val(valorSelecionado);
+		
+	});	
+	$('.NFSelectTarget, .NFSelectOptions').mouseleave(function(){
+		$('.NFSelectTarget:visible').hide();
+	})
+
+});
