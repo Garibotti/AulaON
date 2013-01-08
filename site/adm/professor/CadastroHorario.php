8c94f534f5dbc468a666972ff5f5d413c9546969
Giuliano Wagner de Souza <giulianows@gmail.com>
2013-01-08 20:46:38 -0200

    Alteração nome de página e evoluções na mesma
    De "domingo.php" para "cadastrohorario.php"

--- site/adm/professor/CadastroHorario.php
+++ site/adm/professor/CadastroHorario.php
@@ -9,21 +9,32 @@
     	$(function() {
         	$( ".datepicker" ).timepicker();
     	});
-    </script>   
+    </script>
+   
 </head>
 
 <body>
     
-    <p>Adicione aqui seus horários para Domingo</p><br>
-    <input type="text" name="tab" id="tab" class="datepicker" /> 
+    <p>Adicione aqui sua grade de horários para Domingo</p><br>
+    Nome da Grade:<input type="text" name="" id="" class="" /><br><br>
+    Início:<input type="text" name="tab" id="tab" class="datepicker" /> 
+    Término:<input type="text" name="tab2" id="tab2" class="datepicker" />
     <button id="incluir" onClick="afixar()">Incluir</button>
-    <button id="incluir" onClick="">Incluir</button>  
+    <button id="salvar" onClick="">Salvar</button>  
     <ul id="tabela"></ul>
 
 	<script>
 		function afixar() { 
-		var title = $("#tab").val();
-		$("#tabela").append("<li>" + title + "</li>");
+			var title = $("#tab").val();
+			var title2 = $("#tab2").val();
+			if(title < title2 || title != title2){
+				
+				
+				$("#tabela").append("<li class='listahi'>" + title + "</li>" + "<li class='listahf'>" + title2 + "</li>");
+				$("#tab").val("");
+				$("#tab2").val("");
+			}
+			else{alert('Horário de início maior que horário de término');}
 		}
 	</script>
 
